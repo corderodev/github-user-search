f5a1e94fb2af03c7bfa6973255f7d3031c692726
Daniel Cordero <dn.lambb@gmail.com>
2022-12-04 01:50:07 -0300

    feat(Navbar): :sparkles: add new Navbar component
    The Navbar contains links to my portfolio, my github profile and the repository for this project.

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,32 @@
+export const Navbar = () => {
+  return (
+    <header className="w-full h-fit min-h-20 bg-transparent overflow-hidden text-gh-white px-6 py-4 flex xs:justify-center md:justify-end absolute">
+      <ul className="flex gap-5 md:gap-8 text-lg w-fit">
+        <li>
+          <a
+            href="https://dnlambb.com"
+            className="hover:text-gh-green transition-all"
+          >
+            Portfolio
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://github.com/dnlambb"
+            className="hover:text-gh-green transition-all"
+          >
+            Github
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://github.com/dnlambb/github-user-search"
+            className="hover:text-gh-green transition-all"
+          >
+            Repository
+          </a>
+        </li>
+      </ul>
+    </header>
+  );
+};
